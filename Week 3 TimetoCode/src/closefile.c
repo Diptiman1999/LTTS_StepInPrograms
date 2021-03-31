8f8cfbb51000868f3b674b28b45d301817f2c553
Diptiman1999 <diptiman.sen1999@gmail.com>
2021-03-31 16:56:13 +0530

    Week 3 time to code completed

--- Week 3 TimetoCode/src/closefile.c
+++ Week 3 TimetoCode/src/closefile.c
@@ -1,12 +1,10 @@
 #include "file_header.h"
 
-error_t openfile(FILE *ptr)
+error_t closefile(FILE *ptr)
 {
     if(ptr==NULL)
         return FILE_NOT_FOUND;
     
     fclose(ptr);
-    if(ptr==NULL)
-        return SUCCESS;
-    return FAILURE;
+    return SUCCESS;
 }
--- Week 3 TimetoCode/test/test_main.c
+++ Week 3 TimetoCode/test/test_main.c
@@ -8,13 +8,20 @@ void test_openfile(void)
 {
     TEST_ASSERT_EQUAL(SUCCESS,openfile(fopen("data.csv","r")));
 }
+void test_closefile(void)
+{
+    FILE *ptr=fopen("data.csv","r");
+    TEST_ASSERT_EQUAL(SUCCESS,closefile(ptr));
+
+}
 void test_storedata(void)
 {
-    FILE *fptr=fopen("dat.csv","r");
+    FILE *fptr=fopen("data.csv","r");
     int data_count;
     info_t *data;
     data=(info_t*)(calloc(5,sizeof(info_t)));
     TEST_ASSERT_EQUAL(SUCCESS,storedata(fptr,data,&data_count));
+    fclose(fptr);
 }
 int test_main()
 {
@@ -25,6 +32,7 @@ int test_main()
     //Run test Function
     RUN_TEST(test_openfile); 
     RUN_TEST(test_storedata); 
+    RUN_TEST(test_closefile);
   
     //Closed the Unity Test Framework
     return UNITY_END();
