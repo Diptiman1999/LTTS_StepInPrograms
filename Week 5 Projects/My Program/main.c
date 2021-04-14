7464feb619ea762d5ad4428f0dbc9b43fc99d8f4
Diptiman1999 <diptiman.sen1999@gmaail.com>
2021-04-14 11:19:05 +0530

    main login problem solved

--- Week 5 Projects/My Program/main.c
+++ Week 5 Projects/My Program/main.c
@@ -3,6 +3,6 @@ int main()
 {
     error_t status_initialise = initialise();
     error_t status_welcome=welcomeMessage();
-    error_t status_login=log in();
+    error_t status_login=login();
     return 0;
 }
